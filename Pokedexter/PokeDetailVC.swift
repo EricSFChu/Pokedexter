6641cdeb274fa77becfbfb8a31d3f184d2b9b6d1
EricSFChu <echu6@mail.ccsf.edu>
2017-05-21 05:47:24 -0700

    Added readme.md. Added UIKit dynamics for physics effects.

--- Pokedexter/PokeDetailVC.swift
+++ Pokedexter/PokeDetailVC.swift
@@ -13,7 +13,14 @@ class PokeDetailVC: UIViewController {
     
     var pokemon: Pokemon!
     var musicPlayer: AVAudioPlayer!
+    var alternateMusicPlayer: AVAudioPlayer!
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    var collision: UICollisionBehavior!
+    var itemBehavior: UIDynamicItemBehavior!
 
+    @IBOutlet weak var navBar: UIView!
+    @IBOutlet weak var typeLabel: UILabel!
     @IBOutlet weak var pokemonNameLabel: UILabel!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var mainImage: UIImageView!
@@ -35,16 +42,32 @@ class PokeDetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         pokemonNameLabel.text = pokemon.name.capitalized
+        
         mainImage.layer.cornerRadius = 5.0
         evolutionBar.layer.cornerRadius = 5.0
+        
+        initAudio()
         
         pokemon.downloadDetails {
             
             //code will be called after network call is complete
             self.updateUI()
         }
-        
     }
+    
+    func initAudio(){
+        let path = Bundle.main.path(forResource: "GangnamStyle", ofType: "mp3")
+        
+        do {
+            alternateMusicPlayer = try AVAudioPlayer(contentsOf: URL(string: path!)!)
+            alternateMusicPlayer.prepareToPlay()
+            alternateMusicPlayer.numberOfLoops = -1
+
+        } catch let err as NSError {
+            NSLog(err.debugDescription)
+        }
+    }
+    
 
     func updateUI(){
         mainImage.image = UIImage(named: "\(pokemon.pokedexId)")
@@ -60,6 +83,7 @@ class PokeDetailVC: UIViewController {
         pokemonSpeedLabel.text = pokemon.speed
         pokemonHealthLabel.text = pokemon.health
         evolutionLabel.text = pokemon.evolutionText
+        
         if pokemon.evolvedId == "none" {
             evolutionImage.isHidden = true
         } else {
@@ -80,6 +104,43 @@ class PokeDetailVC: UIViewController {
         } else {
             musicPlayer.play()
             sender.alpha = 1
+        }
+    }
+    @IBAction func segmentedControlDidChange(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 1 {
+            animator = UIDynamicAnimator(referenceView: view)
+            gravity = UIGravityBehavior(items: [secondaryImage])
+            collision = UICollisionBehavior(items: [secondaryImage, evolutionBar])
+            
+            animator.addBehavior(gravity)
+            animator.addBehavior(self.collision)
+            
+            collision.translatesReferenceBoundsIntoBoundary = true
+            itemBehavior = UIDynamicItemBehavior(items: [secondaryImage])
+            itemBehavior.elasticity = 1.2
+            
+            animator.addBehavior(itemBehavior)
+            
+            UIView.animate(withDuration: 0.6, delay: 0, options: [.curveEaseInOut, .autoreverse, .repeat], animations: {self.mainImage.alpha = 0.5 }, completion: nil)
+            UIView.animate(withDuration: 0.6, delay: 0, options: [.curveEaseInOut, .autoreverse, .repeat], animations: {self.evolutionBar.alpha = 0.5 }, completion: nil)
+            UIView.animate(withDuration: 0.6, delay: 0.1, options: [.curveEaseInOut, .autoreverse, .repeat], animations: {self.navBar.alpha = 0.5 }, completion: nil)
+            
+            musicPlayer.stop()
+            alternateMusicPlayer.play()
+        } else {
+            animator.removeAllBehaviors()
+            
+            mainImage.layer.removeAllAnimations()
+            mainImage.alpha = 1
+            
+            evolutionBar.layer.removeAllAnimations()
+            evolutionBar.alpha = 1
+            
+            navBar.layer.removeAllAnimations()
+            navBar.alpha = 1
+            
+            alternateMusicPlayer.stop()
+            musicPlayer.play()
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,12 @@
+#Pokedexter App
+
+-This application leverages PokeAPI
+
+-With this app you will get all the information on 718 Pokemon you want.
+They will even show you some cool moves.
+
+-This app leverages AVFoundation for audio and UIKit Dynamics for physics
+
+[![Video](https://img.youtube.com/vi/Kr78lD36W5w/0.jpg)](https://www.youtube.com/watch?v=Kr78lD36W5w)
+
+*EricDev 2017
